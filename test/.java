8bb6fab97a0fa5e2177a111953ec6d7d7d936b50
TapSoftteam <tapsoftsuport@gmail.com>
2017-06-02 18:34:20 +0300

    Create .java

---
+++ test/.java
@@ -0,0 +1,10 @@
+public class calkulator {
+    
+    public static void main(String[] args) {
+        system.out.println("calculate...");
+        int fir = integer.valueof(arg[0]);
+        int sec = integer.valueof(arg[1]);
+        int sum = fir + sec;
+        system.out.print("sum is = " + sum);
+    }
+}
